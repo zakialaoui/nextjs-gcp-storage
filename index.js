5f6ac835182418e16b4267ab0913767a4a7f33ce
zakialaoui <38666335+zakialaoui@users.noreply.github.com>
2021-04-09 12:35:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-'use strict';
-const pulumi = require('@pulumi/pulumi');
-const gcp = require('@pulumi/gcp');
-
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket('my-bucket', {
-  cors: [
-    {
-      methods: ['POST'],
-      origins: ['*'],
-      responseHeaders: ['*'],
-    },
-  ],
-  forceDestroy: true,
-});
-
-// Export the DNS name of the bucket
-exports.bucketName = bucket.url;
+<img src="https://i.imgur.com/Aksi47y.png">
